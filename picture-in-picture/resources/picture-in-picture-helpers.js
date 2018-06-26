7e7ad4d37c5c4fece450313d5fa7721cf7ab6b2b
François Beaufort <beaufort.francois@gmail.com>
2018-06-26 05:18:18 -0700

    [Picture-in-Picture] Fix web platform tests that time out.
    This makes sure web platform tests for Picture-in-Picture do not time
    out when Picture-in-Picture API is not available.
    
    https://github.com/web-platform-tests/wpt/pull/11329
    
    Bug: 806249
    Change-Id: I6185253dfa4aae5111e87553581b8464032558d1

--- picture-in-picture/resources/picture-in-picture-helpers.js
+++ picture-in-picture/resources/picture-in-picture-helpers.js
@@ -1,16 +1,20 @@
+if (!('pictureInPictureEnabled' in document)) {
+  HTMLVideoElement.prototype.requestPictureInPicture = function() {
+    return Promise.reject('Picture-in-Picture API is not available');
+  }
+}
+
 function callWithTrustedClick(callback) {
-  return new Promise(resolve => {
-    let button = document.createElement('button');
-    button.textContent = 'click to continue test';
-    button.style.display = 'block';
-    button.style.fontSize = '20px';
-    button.style.padding = '10px';
-    button.onclick = () => {
-      document.body.removeChild(button);
-      resolve(callback());
-    };
-    document.body.appendChild(button);
-    test_driver.click(button);
+  return new Promise((resolve, reject) => {
+    let target = document.getElementById('target');
+    if (!target) {
+      target = document.createElement('button');
+      target.setAttribute('id', 'target');
+      document.body.appendChild(target);
+    }
+    test_driver.click(target)
+    .then(_ => resolve(callback()))
+    .catch(_ => reject('Click failed'));
   });
 }
 
